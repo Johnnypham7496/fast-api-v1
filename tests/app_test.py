51d05c769fe4fd0969a2663a210b8bf183e3e1f0
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-08 20:44:16 -0500

    create test for welcome and added pytest.ini

---
+++ tests/app_test.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app
+
+# TestClient: TestClient is a class provided by testing frameworks like pytest or libraries like FastAPI or Flask. 
+# It is designed to simulate HTTP requests and interact with your application as if it were a real client.
+
+# client = TestClient(): This line creates an instance of the TestClient class and assigns it to the variable client. 
+# You can then use this client object to make HTTP requests to your application during testing.
+client = TestClient(app)
+
+
+def test_tc0001_welcome():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, welcome to Autobots FastAPI bootcamp"}
