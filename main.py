9a541873b09ed0a9282e4236bd49c7aeb22143b2
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-08 20:40:12 -0500

    change swagger app title and description

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Response, status
 
+# creating app variable for path operations
+app = FastAPI(
+    title="Johnny's-FastAPI",
+    description="This is the swagger spec for the FastApi workshop",
+    version='1.0.0'
+)
 
-app = FastAPI()
 
-
-@app.get("/", tags="welcome", response_description="Displays welcome message")
+# get path operation for welcome message. Tags and response_description are for swagger ui documentation
+@app.get("/", tags=["Welcome"], response_description="Displays Welcome message")  
 async def welcome(response: Response):
     response.status_code = status.HTTP_200_OK
+#   when a user hits this endpoint, the status_response should be a 200 and will return a message for the user
+#   response parameter inside the function is for HTTP status codes
     return {"message": "Hello, welcome to Autobots FastAPI bootcamp"}
