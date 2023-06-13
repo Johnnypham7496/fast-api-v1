9c0af4037a9e84494731b23dd8341a32d7c017bd
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-13 18:41:59 -0500

    refractor code to handle a parameter that is not found

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ app.include_router(app_router.router)
 app.include_router(users_router.router)
 
 
-@app.get("/dbsetup")
-def create_db(db: Session = Depends(get_db)):
-    user_db.Base.metadata.drop_all(engine)
-    user_db.Base.metadata.create_all(engine)
-    users_repository.add_user_td(db)
-    response_text = '{"message": "Database created."}'
-    response = Response(content=response_text, status_code=200, media_type='application/json')
-    return response
+# @app.get("/dbsetup")
+# def create_db(db: Session = Depends(get_db)):
+#     user_db.Base.metadata.drop_all(engine)
+#     user_db.Base.metadata.create_all(engine)
+#     users_repository.add_user_td(db)
+#     response_text = '{"message": "Database created."}'
+#     response = Response(content=response_text, status_code=200, media_type='application/json')
+#     return response
 
 
 if __name__ == "__main__":
--- router/users_router.py
+++ router/users_router.py
@@ -1,8 +1,8 @@
-from fastapi import Response, Depends, APIRouter, status
+from fastapi import Response, Depends, APIRouter, status, HTTPException
 from repository import users_repository
 from db_config import get_db
 from sqlalchemy.orm import Session
-from schemas import UserModel
+from schemas import UserModel, MessageModel
 from typing import List
 
 router = APIRouter(
@@ -18,8 +18,13 @@ def get_users(resposne: Response, db: Session = Depends(get_db)):
     return return_value
 
 
-@router.get("/{username}", response_description="Display user by username", description="Retrieves user by username", response_model=UserModel)
+@router.get("/{username}", response_description="Display user by username", description="Retrieves user by username", response_model=UserModel, responses= {404: {"model": MessageModel}})
 def get_by_username(response: Response, username: str, db: Session = Depends(get_db) ):
     return_value = users_repository.get_by_username(db, username)
+
+    if return_value == None:
+        response_text = "username not found. Please check your parameter and try again"
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= response_text)
+    
     response.status_code = status.HTTP_200_OK
     return return_value
